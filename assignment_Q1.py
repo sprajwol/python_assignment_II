9a2ce8fe95f8db663d53c0d7cec752b2650d073b
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2020-07-05 14:41:12 +0545

    Q2 done

---
+++ assignment_Q1.py
@@ -0,0 +1,3 @@
+# 1. Create a variable, paragraph, that has the following content:
+# "Python is a great language!", said Fred. "I don't ever remember
+# having this much fun before."
---
+++ assignment_Q2.py
@@ -0,0 +1,19 @@
+# 2. Write an if statement to determine whether a variable holding a year
+# is a leap year.
+
+
+def is_leap(year):
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                print(f"{year} is a leap year.")
+            else:
+                print(f"{year} is not a leap year.")
+
+        else:
+            print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
+
+input_year = int(input("Enter a year:"))
